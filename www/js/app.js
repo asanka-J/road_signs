d974551e0d7ff5ad009de3c63ecf484bef050830
Asanka <asanka.jsndr@gmail.com>
2017-04-17 00:41:48 +0530

    add controllers and framework 7 (photo browser)

--- www/js/app.js
+++ www/js/app.js
@@ -81,20 +81,4 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives
       });
     }
   };
-})
-
-.controller("ExampleController", function($scope) {
-
-    $scope.images = [];
-
-    $scope.loadImages = function() {
-        for(var i = 0; i < 4; i++) {
-            $scope.images.push({id: i, src: "http://assets03.dappei.com/uploads/brand/image/4123/small_876caaf2d1731e2a.jpg"});
-            $scope.images.push({id: i, src: "http://cf.mp-cdn.net/37/1a/7056657a120d473a051831e0a8ac-sometimes-yes-sometimes-no.png"});
-            $scope.images.push({id: i, src: "http://www.minoguesbeverage.com/wp-content/uploads/2013/10/Hobgoblin-100x100.jpg"});
-            $scope.images.push({id: i, src: "http://www.minoguesbeverage.com/wp-content/uploads/2015/01/download-100x100.jpg"});
-            $scope.images.push({id: i, src: "http://www.bateibeer.com/wp-content/uploads/2013/12/mesmerizing-dining-room-vintage-glass-top-42-inch-round-dining-table-sets-with-couple-glass-and-single-beer-bottle-image-100x100.jpg"});
-        }
-    }
-
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('app.controllers', [])
   
-.controller('pageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('pageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/gucaptione/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
@@ -8,75 +8,255 @@ function ($scope, $stateParams) {
 
 }])
    
-.controller('page2Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('page2Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/gucaptione/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, $timeout, $ionicPopup) {
+
+
+    $scope.images = [];
+
+    $scope.loadImages = function() {
+        for(var i = 0; i < 1; i++) {
+            $scope.images.push({caption: "නවතිනු", src: "img/9hIzcbRR1MDxwdUJIiwA_1.PNG"});
+            $scope.images.push({caption: "ඉදිරියට එන්න", src: "img/w0bXAea4SN2GLGR3zzFM_2.PNG"});
+            $scope.images.push({caption: "ඉදිරියෙන් සහ පිටිපසින් එන වාහන නතර වෙනූ", src: "img/sKtF1bLRqqmkNKYCzuoA_3.PNG"});
+            $scope.images.push({caption: "ඉදිරියට එන්න", src: "img/tWk45cj6R4CaLij8BMJC_5.PNG"});
+            $scope.images.push({caption: "පිටිපසින් එන වාහන නතර වෙනූ", src: "img/BvKKBp6Sbe1LLkmgz5Rn_6.PNG"});
+        }
+    }
+
+  $scope.photoBrowser = photoBrowser;
+
+  function photoBrowser(index){
+    photoBrowserStandalone(index, $scope.images)
+  }
+
+  function photoBrowserStandalone(index, images){
+
+    
+    var myApp = new Framework7({
+        init: false, //IMPORTANT - just do it, will write about why it needs to false later
+    });
+    var myPhotoBrowserStandalone = myApp.photoBrowser({
+        type: 'standalone',
+        theme: 'light',
+        photos : images,
+        initialSlide: index,
+        onClose: function(){
+          myApp = undefined;
+        }
+    });
+    myPhotoBrowserStandalone.open();
+  }
 
 
 }])
    
-.controller('page3Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('page3Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/gucaptione/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
+
+
+    $scope.images = [];
+
+    $scope.loadImages = function() {
+        for(var i = 0; i < 4; i++) {
+            $scope.images.push({caption: i, src: "http://assets03.dappei.com/uploads/brand/image/4123/small_876caaf2d1731e2a.jpg"});
+            $scope.images.push({caption: i, src: "http://cf.mp-cdn.net/37/1a/7056657a120d473a051831e0a8ac-sometimes-yes-sometimes-no.png"});
+            $scope.images.push({caption: i, src: "http://www.minoguesbeverage.com/wp-content/uploads/2013/10/Hobgoblin-100x100.jpg"});
+            $scope.images.push({caption: i, src: "http://www.minoguesbeverage.com/wp-content/uploads/2015/01/download-100x100.jpg"});
+            $scope.images.push({caption: i, src: "http://www.bateibeer.com/wp-content/uploads/2013/12/mesmerizing-dining-room-vintage-glass-top-42-inch-round-dining-table-sets-with-couple-glass-and-single-beer-bottle-image-100x100.jpg"});
+        }
+    }
 
 }])
    
-.controller('page4Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('page4Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/gucaptione/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
+
+
+    $scope.images = [];
+
+    $scope.loadImages = function() {
+        for(var i = 0; i < 4; i++) {
+            $scope.images.push({caption: i, src: "http://assets03.dappei.com/uploads/brand/image/4123/small_876caaf2d1731e2a.jpg"});
+            $scope.images.push({caption: i, src: "http://cf.mp-cdn.net/37/1a/7056657a120d473a051831e0a8ac-sometimes-yes-sometimes-no.png"});
+            $scope.images.push({caption: i, src: "http://www.minoguesbeverage.com/wp-content/uploads/2013/10/Hobgoblin-100x100.jpg"});
+            $scope.images.push({caption: i, src: "http://www.minoguesbeverage.com/wp-content/uploads/2015/01/download-100x100.jpg"});
+            $scope.images.push({caption: i, src: "http://www.bateibeer.com/wp-content/uploads/2013/12/mesmerizing-dining-room-vintage-glass-top-42-inch-round-dining-table-sets-with-couple-glass-and-single-beer-bottle-image-100x100.jpg"});
+        }
+    }
 
 }])
    
-.controller('page5Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('page5Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/gucaptione/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
+
+    $scope.images = [];
+
+    $scope.loadImages = function() {
+      
+        for(var i = 0; i < 4; i++) {
+            $scope.images.push({caption: i, src: "http://assets03.dappei.com/uploads/brand/image/4123/small_876caaf2d1731e2a.jpg"});
+            $scope.images.push({caption: i, src: "http://cf.mp-cdn.net/37/1a/7056657a120d473a051831e0a8ac-sometimes-yes-sometimes-no.png"});
+            $scope.images.push({caption: i, src: "http://www.minoguesbeverage.com/wp-content/uploads/2013/10/Hobgoblin-100x100.jpg"});
+            $scope.images.push({caption: i, src: "http://www.minoguesbeverage.com/wp-content/uploads/2015/01/download-100x100.jpg"});
+            $scope.images.push({caption: i, src: "http://www.bateibeer.com/wp-content/uploads/2013/12/mesmerizing-dining-room-vintage-glass-top-42-inch-round-dining-table-sets-with-couple-glass-and-single-beer-bottle-image-100x100.jpg"});
+        }
+    }
+
+}])
+
+
+
+.controller("ExampleController", function($scope, $timeout, $ionicPopup) {
+
+     $scope.images = [];
+
+    $scope.loadImages = function() {
+        for(var i = 0; i < 1; i++) {
+            $scope.images.push({caption: "first", src:  "http://cf.mp-cdn.net/37/1a/7056657a120d473a051831e0a8ac-sometimes-yes-sometimes-no.png"});
+            $scope.images.push({caption: "second", src: "http://cf.mp-cdn.net/37/1a/7056657a120d473a051831e0a8ac-sometimes-yes-sometimes-no.png"});
+            $scope.images.push({caption: "third", src: "http://www.minoguesbeverage.com/wp-content/uploads/2013/10/Hobgoblin-100x100.jpg"});
+            $scope.images.push({caption: "4th", src: "http://www.minoguesbeverage.com/wp-content/uploads/2015/01/download-100x100.jpg"});
+            $scope.images.push({caption: "5th", src: "http://www.minoguesbeverage.com/wp-content/uploads/2015/01/download-100x100.jpg"});
+        }
+    }
+
+      $scope.photoBrowser = photoBrowser;
+
+  function photoBrowser(index){
+    photoBrowserStandalone(index, $scope.images)
+  }
+
+  function photoBrowserStandalone(index, images){
+
+    
+    var myApp = new Framework7({
+        init: false, //IMPORTANT - just do it, will write about why it needs to false later
+    });
+    var myPhotoBrowserStandalone = myApp.photoBrowser({
+        type: 'standalone',
+        theme: 'light',
+        photos : images,
+        initialSlide: index,
+        onClose: function(){
+          myApp = undefined;
+        }
+    });
+    myPhotoBrowserStandalone.open();
+  }
+
+
+
+})
+   
+.controller('page6Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/gucaptione/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+    $scope.images = [];
+
+    $scope.loadImages = function() {
+        for(var i = 0; i < 4; i++) {
+            $scope.images.push({caption: i, src: "http://assets03.dappei.com/uploads/brand/image/4123/small_876caaf2d1731e2a.jpg"});
+            $scope.images.push({caption: i, src: "http://cf.mp-cdn.net/37/1a/7056657a120d473a051831e0a8ac-sometimes-yes-sometimes-no.png"});
+            $scope.images.push({caption: i, src: "http://www.minoguesbeverage.com/wp-content/uploads/2013/10/Hobgoblin-100x100.jpg"});
+            $scope.images.push({caption: i, src: "http://www.minoguesbeverage.com/wp-content/uploads/2015/01/download-100x100.jpg"});
+            $scope.images.push({caption: i, src: "http://www.bateibeer.com/wp-content/uploads/2013/12/mesmerizing-dining-room-vintage-glass-top-42-inch-round-dining-table-sets-with-couple-glass-and-single-beer-bottle-image-100x100.jpg"});
+        }
+    }
 
 }])
    
-.controller('page6Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('page7Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/gucaptione/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
+
+    $scope.images = [];
+
+    $scope.loadImages = function() {
+        for(var i = 0; i < 4; i++) {
+            $scope.images.push({caption: i, src: "http://assets03.dappei.com/uploads/brand/image/4123/small_876caaf2d1731e2a.jpg"});
+            $scope.images.push({caption: i, src: "http://cf.mp-cdn.net/37/1a/7056657a120d473a051831e0a8ac-sometimes-yes-sometimes-no.png"});
+            $scope.images.push({caption: i, src: "http://www.minoguesbeverage.com/wp-content/uploads/2013/10/Hobgoblin-100x100.jpg"});
+            $scope.images.push({caption: i, src: "http://www.minoguesbeverage.com/wp-content/uploads/2015/01/download-100x100.jpg"});
+            $scope.images.push({caption: i, src: "http://www.bateibeer.com/wp-content/uploads/2013/12/mesmerizing-dining-room-vintage-glass-top-42-inch-round-dining-table-sets-with-couple-glass-and-single-beer-bottle-image-100x100.jpg"});
+        }
+    }
 
 }])
    
-.controller('page7Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('page8Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/gucaptione/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
+
+    $scope.images = [];
+
+    $scope.loadImages = function() {
+        for(var i = 0; i < 4; i++) {
+            $scope.images.push({caption: i, src: "http://assets03.dappei.com/uploads/brand/image/4123/small_876caaf2d1731e2a.jpg"});
+            $scope.images.push({caption: i, src: "http://cf.mp-cdn.net/37/1a/7056657a120d473a051831e0a8ac-sometimes-yes-sometimes-no.png"});
+            $scope.images.push({caption: i, src: "http://www.minoguesbeverage.com/wp-content/uploads/2013/10/Hobgoblin-100x100.jpg"});
+            $scope.images.push({caption: i, src: "http://www.minoguesbeverage.com/wp-content/uploads/2015/01/download-100x100.jpg"});
+            $scope.images.push({caption: i, src: "http://www.bateibeer.com/wp-content/uploads/2013/12/mesmerizing-dining-room-vintage-glass-top-42-inch-round-dining-table-sets-with-couple-glass-and-single-beer-bottle-image-100x100.jpg"});
+        }
+    }
 
 }])
    
-.controller('page8Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('page9Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/gucaptione/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
+
+    $scope.images = [];
+
+    $scope.loadImages = function() {
+        for(var i = 0; i < 4; i++) {
+            $scope.images.push({caption: i, src: "http://assets03.dappei.com/uploads/brand/image/4123/small_876caaf2d1731e2a.jpg"});
+            $scope.images.push({caption: i, src: "http://cf.mp-cdn.net/37/1a/7056657a120d473a051831e0a8ac-sometimes-yes-sometimes-no.png"});
+            $scope.images.push({caption: i, src: "http://www.minoguesbeverage.com/wp-content/uploads/2013/10/Hobgoblin-100x100.jpg"});
+            $scope.images.push({caption: i, src: "http://www.minoguesbeverage.com/wp-content/uploads/2015/01/download-100x100.jpg"});
+            $scope.images.push({caption: i, src: "http://www.bateibeer.com/wp-content/uploads/2013/12/mesmerizing-dining-room-vintage-glass-top-42-inch-round-dining-table-sets-with-couple-glass-and-single-beer-bottle-image-100x100.jpg"});
+        }
+    }
 
 }])
    
-.controller('page9Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('page10Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/gucaptione/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
 
-}])
-   
-.controller('page10Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+    $scope.images = [];
 
+    $scope.loadImages = function() {
+        for(var i = 0; i < 4; i++) {
+            $scope.images.push({caption: i, src: "http://assets03.dappei.com/uploads/brand/image/4123/small_876caaf2d1731e2a.jpg"});
+            $scope.images.push({caption: i, src: "http://cf.mp-cdn.net/37/1a/7056657a120d473a051831e0a8ac-sometimes-yes-sometimes-no.png"});
+            $scope.images.push({caption: i, src: "http://www.minoguesbeverage.com/wp-content/uploads/2013/10/Hobgoblin-100x100.jpg"});
+            $scope.images.push({caption: i, src: "http://www.minoguesbeverage.com/wp-content/uploads/2015/01/download-100x100.jpg"});
+            $scope.images.push({caption: i, src: "http://www.bateibeer.com/wp-content/uploads/2013/12/mesmerizing-dining-room-vintage-glass-top-42-inch-round-dining-table-sets-with-couple-glass-and-single-beer-bottle-image-100x100.jpg"});
+        }
+    }
 
 }])
  
