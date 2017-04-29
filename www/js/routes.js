93bd26d19a4fa940721de4dca5e3b6a39d83e908
Asanka <asanka.jsndr@gmail.com>
2017-04-29 16:58:21 +0530

    minor html change

--- www/js/routes.js
+++ www/js/routes.js
@@ -69,11 +69,7 @@ angular.module('app.routes', [])
     templateUrl: 'templates/page10.html',
     controller: 'page10Ctrl'
   })
-  .state('test', {
-    url: '/test',
-    templateUrl: 'templates/test.html',
-    
-  })
+  
 
 
 
